ce789c62ab902a0abc5f0d5922282152b6e49b51
damongeorgiou <dngx113@gmail.com>
2019-01-17 12:47:58 -0500

    create Static group and create Constants.swift file inside of it; update Constants with strings for CameraAccesAlertControllers title and message fields

---
+++ StrawberryOrNah/Static/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  StrawberryOrNah
+//
+//  Created by Damon Georgiou on 1/17/19.
+//  Copyright © 2019 Damon Georgiou. All rights reserved.
+//
+
+struct Constants {
+    struct AllowCameraAccessAlert {
+        static let TITLE = "StrawberryOrNah Would Like To Access the Camera"
+        static let MESSAGE = "StrawberryOrNah needs camera access to take photos and identify objects."
+    }
+}
