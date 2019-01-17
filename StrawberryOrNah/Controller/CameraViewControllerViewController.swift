0fb4b1586741be5baa8a0fa5a22d8f8012375022
damongeorgiou <dngx113@gmail.com>
2019-01-17 12:43:43 -0500

    implement basic setup and running of AVFoundation capture session

--- StrawberryOrNah/Controller/CameraViewControllerViewController.swift
+++ StrawberryOrNah/Controller/CameraViewControllerViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
     
@@ -18,10 +19,83 @@ class CameraViewController: UIViewController {
     @IBOutlet weak var confidenceLbl: UILabel!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    private var captureSession: AVCaptureSession!
+    private var backCamera: AVCaptureDevice?
+    private var captureDeviceInput: AVCaptureDeviceInput?
+    private var cameraOutput: AVCapturePhotoOutput!
+    private var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    private var photoData: Data?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        setupCaptureSession()
+        setupDevice()
+        setupInputOutput()
+        setupPreviewLayer()
+        startRunningCaptureSession()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        stopRunningCaptureSession()
+    }
+    
+    func setupCaptureSession() {
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+    }
+    
+    func setupDevice() {
+        backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+    }
+    
+    func setupInputOutput() {
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            if captureSession.canAddInput(input) {
+                captureSession.addInput(input)
+            }
+            
+            cameraOutput = AVCapturePhotoOutput()
+            if captureSession.canAddOutput(cameraOutput) {
+                captureSession.addOutput(cameraOutput)
+            }
+        } catch {
+            debugPrint(error)
+        }
+    }
+    
+    func setupPreviewLayer() {
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+        previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        previewLayer.frame = cameraView.bounds
+        
+        cameraView.layer.addSublayer(previewLayer)
+    }
+    
+    func startRunningCaptureSession() {
+        if !captureSession.isRunning {
+            captureSession.startRunning()
+        }
+    }
+    
+    func stopRunningCaptureSession() {
+        if captureSession.isRunning {
+            captureSession.stopRunning()
+            if captureSession.inputs.count > 0 {
+                captureSession.removeInput(captureDeviceInput!)
+            }
+            if captureSession.outputs.count > 0 {
+                captureSession.removeOutput(cameraOutput!)
+            }
+        }
+    }
+    
 
     @IBAction func flashBtnWasPressed(_ sender: Any) {
     }
